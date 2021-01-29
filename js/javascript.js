0dd4b5cc1a83343994723503d326a786e1b48403
MeriMurata <meri@dodopey.com>
2021-01-29 23:42:58 +0900

    first update

---
+++ js/javascript.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.c-title--menu, .c-overlay, .c-close-button').click(function(){
+        $('.l-sidebar').toggleClass('open');
+        $('.c-overlay').toggleClass('open');
+    });
+
+
+});
+
+
